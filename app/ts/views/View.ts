cb42538a067943499784349b36f39f6bbfded0dd
Lais Fernandes Fialho <laisffn@hotmail.com>
2021-03-05 16:34:46 -0300

    Validando template e parâmetro opcional

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,15 +1,21 @@
 export abstract class View<T> {
 
     private _elemento: JQuery;
+    private _escapar: boolean;
 
-    constructor(seletor: string) {
+    constructor(seletor: string, escapar?: boolean) {
 
         this._elemento = $(seletor); //usando JQuery
+        this._escapar = escapar;
     }
 
     update(model: T): void {
+        let template = this.template(model);
 
-        this._elemento.html(this.template(model));
+        if (this._escapar)
+            template = template.replace(/<script>[\s\S]*?<\/script>/g, '';)
+
+        this._elemento.html(template);
     }
 
     abstract template(model: T): string
